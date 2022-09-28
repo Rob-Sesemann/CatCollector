983934fefab61820b6eeaa717e7e8064ef21fff5
sesemann <robsesemann@gmail.com>
2022-09-28 14:57:49 +0100

    feeding and meal in one to many

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,11 @@ from django.urls import reverse
 
 # Create your models here.
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -10,9 +15,17 @@ class Cat(models.Model):
     age = models.IntegerField()
     image = models.ImageField(upload_to="main_app/static/uploads/", default="")
 
+    # Overriding
     def get_absolute_url(self):
         return reverse('detail', kwargs = {'cat_id': self.id})
 
+    def __str__(self):
+        return self.name
+
 class Feeding(models.Model):
-    date = models.DateField()
-    meal = models.CharField(max_length=1) # B L D
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0]) # B L D
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
